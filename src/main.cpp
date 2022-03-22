b93e2a665c612199f02dbf8610ec71774cee49af
Jeddy854 <jeddytina@gmail.com>
2022-03-22 21:41:27 +0800

    fix some error

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include "yolo_server.h"
 
+using namespace cv;
+
 int main(int argc, char *argv[]){
     ros::init(argc, argv, "yolo1");
     Yolo *yolov4;
     yolov4 = Yolo::GetYolo();
     cv::Mat colorimage;
-    colorimage = yolov4->Get_RGBimage();
-    cv::imshow("Color Image", colorimage);
+    while(true)
+    {
+        colorimage = yolov4->Get_RGBimage();
+        cv::imshow("Color Image", colorimage);
+        wiatKey(30);
+    }
     return 0;
 }
